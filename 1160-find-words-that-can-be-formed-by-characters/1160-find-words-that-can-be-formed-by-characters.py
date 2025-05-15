7c97d302738e426a4d7711923738ddaed71d7058
Daniel Lam <dltlam1987@gmail.com>
2025-05-15 13:06:13 -0700

    Time: 499 ms (5.02%), Space: 18.7 MB (9.7%) - LeetHub

--- 1160-find-words-that-can-be-formed-by-characters/1160-find-words-that-can-be-formed-by-characters.py
+++ 1160-find-words-that-can-be-formed-by-characters/1160-find-words-that-can-be-formed-by-characters.py
@@ -1,6 +1,33 @@
-from collections import defaultdict
+from collections import Counter
 class Solution:
     def countCharacters(self, words: List[str], chars: str) -> int:
-        print(chars)
-        charsDict = defaultdict()
-        print(charsDict)
+        total_length = 0
+
+        for word in words:
+            number_of_deletes = 0
+            charsCounter = Counter(chars)
+            charWordCounter = list(word)
+            print(charsCounter)
+            print(charWordCounter)
+            for char in charWordCounter:
+                if char not in charsCounter:
+                    break
+                else:
+                    print("char", char)
+                    print("char count", charsCounter[char])
+                    charsCounter[char] -= 1
+                    print("char count", charsCounter[char])
+                    if charsCounter[char] == 0:
+                        print("deleting char", char)
+                        del charsCounter[char]
+                    number_of_deletes += 1
+                    print("number_of_deletes", number_of_deletes)
+            # print("word", word)
+            # print("number_of_deletes", number_of_deletes)
+            if number_of_deletes == len(word):
+                total_length += number_of_deletes
+        print(total_length)
+        return total_length
+
+
+
