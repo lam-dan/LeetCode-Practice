27873b029d42aa1d0b83f950e0a584de50d655e4
Daniel Lam <dltlam1987@gmail.com>
2025-05-19 15:52:33 -0700

    Time: 571 ms (32.88%), Space: 18 MB (57.18%) - LeetHub

--- 0259-3sum-smaller/0259-3sum-smaller.py
+++ 0259-3sum-smaller/0259-3sum-smaller.py
@@ -1,38 +1,20 @@
 class Solution:
     def threeSumSmaller(self, nums: List[int], target: int) -> int:
         nums.sort()
-        result = []
+        count = 0
 
         for i in range(len(nums)):
-            if i > 0 and nums[i] == nums[i -1]:
-                continue
             left = i + 1
             right = len(nums) - 1
 
             # Sliding window technique
             while left < right:
                 total = nums[i] + nums[left] + nums[right]
-
-                # if total < target:
-                #     result.append([nums[i], nums[left], nums[right]])
-                #     left += 1
-                #     right -= 1
-                #     # Handle duplicate solutions
-                #     while left < right and nums[left] == nums[left-1]:
-                #         left += 1
-                #     while left < right and nums[right] == nums[right+1]:
-                #         right -= 1
-                # condition when total is > greater than target
-                if total > target:
-                    right -= 1
-                    # while left < right and nums[left] == nums[left-1]:
-                    #     left += 1
-                    # while left < right and nums[right] == nums[right+1]:
-                    #     right -= 1
-                else:
-                    result.append([nums[i], nums[left], nums[right]])
+                if total < target:
+                    count += right - left
                     left += 1
-                
-        return len(result)
+                else:
+                    right -= 1
+        return count
                 
                 
