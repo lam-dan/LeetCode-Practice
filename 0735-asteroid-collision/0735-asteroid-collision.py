d09a41860218f415c23119fabc5d874734dcb800
Daniel Lam <dltlam1987@gmail.com>
2025-05-19 23:56:47 -0700

    Time: 5 ms (71.67%), Space: 18.8 MB (61.78%) - LeetHub

---
+++ 0735-asteroid-collision/0735-asteroid-collision.py
@@ -0,0 +1,40 @@
+class Solution:
+    def asteroidCollision(self, asteroids: List[int]) -> List[int]:
+        # Relative position in space based on index
+        # absolute value = size
+        # sign represents direction (+, right), (-, left)
+        # two asteroids meet, smaller will explodes
+        # if the same size, both explode
+        # else asteroids never meet
+
+        # (5, right) size 5
+        # (10, right) size 10
+
+        # (-5, left) size 5 (deleted)
+
+        # Stack for processing
+        # compare each value before putting it into the stack
+        stack = []
+
+        for current in asteroids:
+            # Incoming left-moving asteroid
+            while stack and stack[-1] > 0 and current < 0:
+                if stack[-1] < abs(current):
+                    stack.pop() # top asteroid in stack explodes
+                    continue # keep checking current asteroid against next top asteroid
+                # if the size of asteroids are the same between top and incoming
+                elif stack[-1] == abs(current):
+                    stack.pop() # top asteroid in stack explodes
+                break # current asteroid also explodes
+            else:
+                # Either stack is empty or no collision -> safe to add
+                stack.append(current)
+        return stack
+
+                
+                
+
+            
+
+
+
