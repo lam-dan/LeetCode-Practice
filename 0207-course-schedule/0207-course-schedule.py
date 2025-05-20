4e742fe8fcfea9f1303ad97803f37ca712116782
Daniel Lam <dltlam1987@gmail.com>
2025-05-20 03:28:55 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,31 +1,56 @@
 from collections import defaultdict, deque
+
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        adj = defaultdict(list)        
-        ndegree = [0] * numCourses #index is used for tracking nodes 
+        if len(prerequisites) == 0:
+            return True
+        # Create adjacency list
+        adj_list = defaultdict(list)
+        # Create an ndegree to keep track of all edges for each node
+        ndegree = [0] * numCourses
+        print(ndegree)
+
 
         for u,v in prerequisites:
-            adj[v].append(u)
+            adj_list[v].append(u)
             ndegree[u] += 1
 
-        # BFS
-        q = deque()
-        
-        for node in range(numCourses):
+        print(adj_list)
+        print(ndegree)
+
+        # Create a queue
+        queue = deque([])
+        print(queue)
+        # Add into the queue the nodes that don't have ndegrees
+        for node in adj_list:
             if ndegree[node] == 0:
-                q.append(node)
+                queue.append(node)
+        
+        print(queue)
+        counter = 0
 
+        # Create a global counter
 
-        visited = 0 #global counter
+        while queue:
+            node = queue.popleft()
+            counter += 1
 
-        while q:
-            node = q.popleft() # node val
-            visited += 1
-
-            for nei in adj[node]:
+            for nei in adj_list[node]:
                 ndegree[nei] -= 1
                 if ndegree[nei] == 0:
-                    q.append(nei)
+                    queue.append(nei)
+        print(counter)
 
-        return numCourses == visited
+        return counter == numCourses
 
+        # Iterate through the queue
+        # Remove the values from queue
+        # Increment the counter
+        # Iterate over neighbors
+        # For each of the neighbors we're going decrement the degrees
+        # now if the neighbor's ndegree is 0, we go ahead and add it to the queue
+
+
+        # Check if the counter is equal to the number of courses
+        # return true
+        # else return false
