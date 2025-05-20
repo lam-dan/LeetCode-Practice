fe15521fec87876ff3e2cd69cfd233f6c2e01ff8
Daniel Lam <dltlam1987@gmail.com>
2025-05-20 12:49:50 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1604-alert-using-same-key-card-three-or-more-times-in-a-one-hour-period/1604-alert-using-same-key-card-three-or-more-times-in-a-one-hour-period.py
@@ -0,0 +1,23 @@
+class Solution:
+    def alertNames(self, keyName: List[str], keyTime: List[str]) -> List[str]:
+        access_history = defaultdict(list)
+
+        for name, time in zip(keyName, keyTime):
+            clock = time.split(":")
+            hours, minutes = int(clock[0]), int(clock[1])
+            total_minutes = hours * 60 + minutes
+            access_history[name].append(total_minutes)
+
+        result = []
+
+        for name, time in access_history.items():
+            time.sort()
+            left = 0
+            for i in range(len(time)):
+                while time[i] - time[left] > 60:
+                    left += 1
+                access_count = i - left + 1
+                if access_count >= 3:
+                    result.append(name)
+        return sorted(result)
+                
