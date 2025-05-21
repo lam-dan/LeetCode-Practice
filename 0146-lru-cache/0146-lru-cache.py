5dcf89ead22eb433fb8bb94831a2efae2f2a857c
Daniel Lam <dltlam1987@gmail.com>
2025-05-21 05:48:14 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,67 +1,77 @@
-from typing import Any
 class Node:
     def __init__(self, key, value):
-        self.key, self.value = key, value
-        self.prev = self.next = None
+        self.key = key
+        self.value = value
+        self.next = None
+        self.prev = None
 
 class LRUCache:
     def __init__(self, capacity: int):
-        self.cache = {} #Initialize with empty, avoid using defaultdict since we don't want default intializes
         self.capacity = capacity
-        self.head = Node(-1,-1)
-        self.tail = Node(-1,-1)
-
-        self.left, self.right = Node(0,0), Node(0,0)
+        self.cache = {}
+        self.tail = Node(None, None)
+        self.head = Node(None, None)
         self.head.next = self.tail
         self.tail.prev = self.head
 
-    # Return value of the key if it exists otherwise, return -1
-    def get(self, key) -> Any:
+    # O(1) operation since we're accessing the dictionary
+    def get(self, key: int) -> int: 
         if key in self.cache:
             node = self.cache[key]
-            self.delete(node)
-            self.insert_after_head(node)
+            # Add moving node to the beginning of the head because it's been recently used
+            self.delete(node) # delete node
+            self.insert_after_head(node) # move node to the head
             return node.value
-        else: 
+        else:
             return -1
 
-    def delete(self, node: Node):
-        node_previous = node.prev
-        node_after = node.next
-        node_previous.next = node_after
-        node_after.prev = node_previous
-
-
-    def insert_after_head(self, node: Node):
-        current_after_head = self.head.next
-        node.next = current_after_head
-        node.prev = self.head
-        self.head.next = node
-        current_after_head.prev = node
-
-    # O(1) capacity
-    # Check if we don't have space, remove the least recently used
-    # otherwise do nothing
-    # Put the value in the dictionary
+    # Put operation update a value in the cache if exists
+    # Put operation to add value 
+    # capacity
     def put(self, key: int, value: int) -> None:
+        size = self.get_size()
+        # 2 Cases
+        # Eviction where size is greater than capacity
+        # Remove a node from the tail
+        if size >= self.capacity:
+            # Least recently used is not self.tail because a doubly linked list
+            # has dummy nodes at head and tail
+            node = self.tail.prev
+            del self.cache[node.key]
+            self.delete(node)
+        # Not greater than capacity
+        # We can put a value in the dictionary and also update it
+        # to be recently used by moving it to the head
         if key in self.cache:
+            # Update the value in the cache
             node = self.cache[key]
             node.value = value
             self.delete(node)
             self.insert_after_head(node)
-        # If not in the map
-        if key not in self.cache:
-            # Remove the least recently used
-            # Multiple Conditions:
-            # 1) Cache is filled up
-            if len(self.cache) >= self.capacity:
-                node = self.tail.prev
-                print(node.key)
-                del self.cache[node.key]
-                self.delete(node)
+        else:
             node = Node(key, value)
             self.cache[key] = node
             self.insert_after_head(node)
 
+    def insert_after_head(self, node: Node):
+        tmp = self.head.next
+        node.next = tmp
+        node.prev = self.head
+        self.head.next = node
+        tmp.prev = node
 
-        
+    def delete(self, node: Node):
+        node_prev = node.prev
+        node_after = node.next
+        node_prev.next = node_after
+        node_after.prev = node_prev
+
+    def get_size(self) -> int:
+        return len(self.cache)
+
+
+    
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
