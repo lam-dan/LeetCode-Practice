18c061ae095f943b6a4fc1f02b0a3eb8d8604225
Daniel Lam <dltlam1987@gmail.com>
2025-06-26 21:26:05 -0700

    Time: 583 ms (23.6%), Space: 18.3 MB (19.05%) - LeetHub

---
+++ 0282-expression-add-operators/0282-expression-add-operators.py
@@ -0,0 +1,27 @@
+class Solution:
+    def addOperators(self, num: str, target: int) -> List[str]:
+
+        def dfs(i: int = 0, prefix: str = '', value: int = 0, prev: int = 0):
+            # Base Case
+            if i == len(num) and value == target:
+                result.append(prefix)
+                return None
+
+            # Try all possible combinations
+            for j in range(i + 1, len(num) + 1):
+                string = num[i:j] # Take the substring num[i:j]
+                number = int(string)
+                 # Skip numbers with leading zeros, except for '0' itself
+                if len(string) > 1 and num[i] == '0': continue
+                if not prefix:
+                    dfs(j, string, number, number)
+                else:
+                    dfs(j, prefix + '+' + string, value + number, number)
+                    dfs(j, prefix + '-' + string, value - number, -number)
+                    dfs(j, prefix + '*' + string, value - prev + prev * number, prev * number)
+        result = []
+
+        dfs()
+        return result
+
+
