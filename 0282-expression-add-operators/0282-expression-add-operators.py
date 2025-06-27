afe21b9236a912a07e1ba0092c0f1f51d1985574
Daniel Lam <dltlam1987@gmail.com>
2025-06-26 23:22:48 -0700

    Time: 545 ms (33.26%), Space: 18.4 MB (7.19%) - LeetHub

--- 0282-expression-add-operators/0282-expression-add-operators.py
+++ 0282-expression-add-operators/0282-expression-add-operators.py
@@ -1,24 +1,52 @@
 class Solution:
     def addOperators(self, num: str, target: int) -> List[str]:
         result = []
-        def dfs(i: int = 0, prefix: str = '', value: int = 0, prev: int = 0):
-            # Base Case
-            if i == len(num) and value == target:
-                result.append(prefix)
-                return None
+        N = len(num)
 
-            # Try all possible combinations
-            for j in range(i + 1, len(num) + 1):
-                string = num[i:j] # Take the substring num[i:j]
-                number = int(string)
-                 # Skip numbers with leading zeros, except for '0' itself
-                if len(string) > 1 and num[i] == '0': continue
-                if not prefix:
-                    dfs(j, string, number, number)
+        def dfs(i: int = 0, value: int = 0, prev: int = 0, expr: list = []):
+            """
+            i: Current index in num
+            value: Evaluated total so far
+            prev: Last operand (for multiplication precedence)
+            expr: List of characters building the expression
+            """
+            if i == N:
+                if value == target:
+                    result.append(''.join(expr))
+                return
+
+            for j in range(i + 1, N + 1):
+                part = num[i:j]
+                number = int(part)
+
+                # Skip numbers with leading zeros
+                if len(part) > 1 and part[0] == '0':
+                    continue
+
+                if i == 0:
+                    # First operand, no operator
+                    expr.extend(part)
+                    dfs(j, number, number, expr)
+                    del expr[-len(part):]
                 else:
-                    dfs(j, prefix + '+' + string, value + number, number)
-                    dfs(j, prefix + '-' + string, value - number, -number)
-                    dfs(j, prefix + '*' + string, value - prev + prev * number, prev * number)
+                    # Addition
+                    expr.append('+')
+                    expr.extend(part)
+                    dfs(j, value + number, number, expr)
+                    del expr[-(len(part) + 1):]
+
+                    # Subtraction
+                    expr.append('-')
+                    expr.extend(part)
+                    dfs(j, value - number, -number, expr)
+                    del expr[-(len(part) + 1):]
+
+                    # Multiplication
+                    expr.append('*')
+                    expr.extend(part)
+                    dfs(j, value - prev + prev * number, prev * number, expr)
+                    del expr[-(len(part) + 1):]
+
         dfs()
         return result
 
