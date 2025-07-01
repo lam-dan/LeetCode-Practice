af79c158642a82884e4d9fb1a1e4d3745013a5c6
Daniel Lam <dltlam1987@gmail.com>
2025-07-01 05:47:01 -0700

    Time: 54 ms (5.3%), Space: 20.3 MB (93.41%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -1,22 +1,19 @@
+from collections import Counter
+
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        # Two Sum (Hash Map) + Prefix Sums (List)
-        count = 0
-        total = 0
+        # Create hashmap for storing prefix_sum:frequency
+        prefix_sums = Counter()
+        prefix_sums[0] = 1 # Initialize empty array prefix_sum as frequency 1
+        curr_sum = 0 # cumulative sum at current index
+        count = 0 # count of subarrays
 
-        hash_map = defaultdict(int)
-        hash_map[0] = 1
-
-        # prefixSum = [nums[0]]
-        for i in range(0, len(nums)):
-            total += nums[i]
-            # prefixSum.append(prefixSum[-1] + nums[i])
-            count += hash_map[total - k]
-            hash_map[total] += 1
+        for num in nums:
+            curr_sum += num
+            complement = curr_sum - k # complement of diff between current cumulative sum minus k
+            if complement in prefix_sums: #if you've seen complement before
+                count += prefix_sums[complement] # increase the count
+            prefix_sums[curr_sum] += 1 # increment counter by one for current curr_sum
         return count
 
-        
 
-        
-
-        print(prefixSum)
