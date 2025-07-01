2d004f9574c4c811d7ed83d7367f76a9af955c15
Daniel Lam <dltlam1987@gmail.com>
2025-07-01 04:32:40 -0700

    Time: 4 ms (1.69%), Space: 18 MB (53.87%) - LeetHub

---
+++ 0339-nested-list-weight-sum/0339-nested-list-weight-sum.py
@@ -0,0 +1,73 @@
+# """
+# This is the interface that allows for creating nested lists.
+# You should not implement it, or speculate about its implementation
+# """
+#class NestedInteger:
+#    def __init__(self, value=None):
+#        """
+#        If value is not specified, initializes an empty list.
+#        Otherwise initializes a single integer equal to value.
+#        """
+#
+#    def isInteger(self):
+#        """
+#        @return True if this NestedInteger holds a single integer, rather than a nested list.
+#        :rtype bool
+#        """
+#
+#    def add(self, elem):
+#        """
+#        Set this NestedInteger to hold a nested list and adds a nested integer elem to it.
+#        :rtype void
+#        """
+#
+#    def setInteger(self, value):
+#        """
+#        Set this NestedInteger to hold a single integer equal to value.
+#        :rtype void
+#        """
+#
+#    def getInteger(self):
+#        """
+#        @return the single integer that this NestedInteger holds, if it holds a single integer
+#        The result is undefined if this NestedInteger holds a nested list
+#        :rtype int
+#        """
+#
+#    def getList(self):
+#        """
+#        @return the nested list that this NestedInteger holds, if it holds a nested list
+#        The result is undefined if this NestedInteger holds a single integer
+#        :rtype List[NestedInteger]
+#        """
+
+class Solution:
+    def depthSum(self, nestedList: List[NestedInteger]) -> int:
+        # test = nestedList.getInteger()
+        # print(nestedList)
+        self.total = 0
+
+        # for ele in nestedList:
+        #     # if ele.isInteger():
+        #     #     self.total += ele.getInteger() * depth
+        #     print("")
+        #     print(ele.getList())
+        #     for nestedList in ele.getList():
+        #         print(nestedList)
+            
+
+        def dfs(nestedList, depth):
+            depth += 1
+
+            for ele in nestedList:
+                if ele.isInteger():
+                    self.total += ele.getInteger() * depth
+                else:
+                    dfs(ele.getList(), depth)
+        dfs(nestedList, 0)
+        return self.total
+
+
+
+        
+        
