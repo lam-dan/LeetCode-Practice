0b2b52c1cbbf13be7f9920fda967e9e1329e4ea6
Daniel Lam <dltlam1987@gmail.com>
2025-07-11 11:41:57 -0700

    Time: 130 ms (41.49%), Space: 40.4 MB (88.4%) - LeetHub

--- 1604-alert-using-same-key-card-three-or-more-times-in-a-one-hour-period/1604-alert-using-same-key-card-three-or-more-times-in-a-one-hour-period.py
+++ 1604-alert-using-same-key-card-three-or-more-times-in-a-one-hour-period/1604-alert-using-same-key-card-three-or-more-times-in-a-one-hour-period.py
@@ -5,22 +5,23 @@ class Solution:
         for name, time in zip(keyName, keyTime):
             clock = time.split(":")
             hours, minutes = int(clock[0]), int(clock[1])
-            total_minutes = hours * 60 + minutes
+            total_minutes = hours * 60 + minutes # convert all to minutes
             access_history[name].append(total_minutes)
 
         result = []
 
-        print(access_history)
 
-        for name, time in access_history.items():
+        for name, time, in access_history.items():
             time.sort()
             left = 0
             for i in range(len(time)):
                 while time[i] - time[left] > 60:
                     left += 1
-                access_count = i - left + 1
+                access_count = i - left + 1 # calculate count based on distance (right pointer - left pointer)
                 if access_count >= 3:
                     result.append(name)
                     break
         return sorted(result)
-                
+
+
+
