0099a2c8a7027c26de4f4d463f186dd8fe477a9c
Daniel Lam <dltlam1987@gmail.com>
2025-07-17 13:27:25 -0700

    Time: 0 ms (100%), Space: 17.8 MB (62.13%) - LeetHub

--- 0791-custom-sort-string/0791-custom-sort-string.py
+++ 0791-custom-sort-string/0791-custom-sort-string.py
@@ -3,12 +3,8 @@ class Solution:
         # We need to reorder the string s so that the characters in order 
         # appear first (and in the given sequence), while any characters in 
         # s not present in order can appear anywhere after.
-        
-        # sorted_order = sorted(order)
 
         new_order = Counter(s)
-        # new_order = set(s)
-        print(new_order)
         result = []
 
         for i in range(len(order)):
@@ -16,13 +12,9 @@ class Solution:
                 result.append(order[i] * new_order[order[i]]) # Append based on frequency count
                 del new_order[order[i]] # delete from dictionary
     
-        print(new_order)
-
-        if len(new_order) > 0:
-            for i in new_order:
-                result.append(i * new_order[i])
-
-        print(result)
+        if len(new_order) > 0: # Any remaining elements in new_order
+            for i in new_order: 
+                result.append(i * new_order[i]) # Append at the end of result multiplied by the frequency
 
         return "".join(result)
             
