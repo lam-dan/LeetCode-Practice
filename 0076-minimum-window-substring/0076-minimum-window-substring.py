e895aa7c69e3e0c2e383985f2431999ca070457f
Daniel Lam <dltlam1987@gmail.com>
2025-07-18 22:20:12 -0700

    Time: 117 ms (39.13%), Space: 28.2 MB (7.05%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -8,23 +8,35 @@ class Solution:
         window_counter = Counter()
         need = len(t_counter)           # Total unique characters needed to satisfy t
 
+        # Preprocess: filter s to only the necessary characters
+        filtered_s = []
         for i in range(len(s)):
-            window_counter[s[i]] += 1  # Include current character in the window
+            if s[i] in t_counter:
+                filtered_s.append((i, s[i]))  # (original index, character)
+
+        for i in range(len(filtered_s)):
+            ch = filtered_s[i][1]
+            window_counter[ch] += 1  # Include current character in the window
 
             # If the current character's count matches the requirement in t, increment count
-            if s[i] in t_counter and window_counter[s[i]] == t_counter[s[i]]:
+            if window_counter[ch] == t_counter[ch]:
                 count += 1
 
             # When all unique required characters are satisfied, try shrinking the window
             while count == need:
+                start = filtered_s[left][0] # Original index in s for the current left boundary
+                end = filtered_s[i][0] # Original index in s for the current right boundary
+
                 # Update minimum window if the current window is smaller
-                if i - left + 1 < minLen:
-                    minLen = i - left + 1
-                    sIndex = left
-                
-                window_counter[s[left]] -= 1 # Shrink the window from the left
-                # # If removing this character breaks the requirement, decrement count
-                if s[left] in t_counter and window_counter[s[left]] < t_counter[s[left]]:
+                if end - start + 1 < minLen:
+                    minLen = end - start + 1
+                    sIndex = start # Save the start index of the current best window
+
+                # Prepare to shrink the window by removing the character at the left boundary
+                left_char = filtered_s[left][1]
+                window_counter[left_char] -= 1
+
+                if window_counter[left_char] < t_counter[left_char]:
                     count -= 1
                 left += 1
         return "" if minLen == float('inf') else s[sIndex:sIndex + minLen]
