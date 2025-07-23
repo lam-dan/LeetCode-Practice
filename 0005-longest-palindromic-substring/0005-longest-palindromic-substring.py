6adc36c9a9a20b5a465255ad75fd513236c0443a
Daniel Lam <dltlam1987@gmail.com>
2025-07-23 04:50:36 -0700

    Time: 253 ms (85.49%), Space: 17.9 MB (44.2%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,21 +1,11 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        
-        # Two-pointer expansion technique from the center
-        def center_expand(left, right):
-            # Expand outward within bounds and characters match 
-            while left >= 0 and right < len(s) and s[left] == s[right]:
-                left -= 1 # Expand left pointer from center
-                right += 1 # Expand right pointer from center
-            # Return the length of the palindrome (overshot by 1 on both sides)
-            return right - left - 1
-
         left = 0
         right = 0
 
         for i in range(len(s)):
-            len1 = center_expand(i,i) # Try expanding around a single character (odd-length palindrome)
-            len2 = center_expand(i, i + 1) # Try expanding around two characters (even-length palindrome)
+            len1 = self.center_expand(s, i, i) # Try expanding around a single character (odd-length palindrome)
+            len2 = self.center_expand(s, i, i + 1) # Try expanding around two characters (even-length palindrome)
 
             max_len = max(len1, len2) # Take longer of the two expansions
             # If this palindrome is longer than the previous one
@@ -33,6 +23,15 @@ class Solution:
                     right = i + half # Update global right pointer (end of palindrome)
 
         return s[left:right + 1] # Return the longest palindrome substring 
+
+    # Two-pointer expansion technique from the center
+    def center_expand(self, s, left, right):
+        # Expand outward within bounds and characters match 
+        while left >= 0 and right < len(s) and s[left] == s[right]:
+            left -= 1 # Expand left pointer from center
+            right += 1 # Expand right pointer from center
+        # Return the length of the palindrome (overshot by 1 on both sides)
+        return right - left - 1
 
         # ---------------------------------------------------------------------
         # Time Complexity: O(n^2)
