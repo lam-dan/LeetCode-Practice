1173c8e19f12467af5d1de14c6ca668a8c28fe19
Daniel Lam <dltlam1987@gmail.com>
2025-07-24 02:33:15 -0700

    Time: 0 ms (100%), Space: 18.9 MB (50.52%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,13 @@
+from collections import defaultdict
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        dic = defaultdict()
+
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement not in dic:
+                dic[num] = i
+            else:
+                return [i, dic[complement]]
+        return []
