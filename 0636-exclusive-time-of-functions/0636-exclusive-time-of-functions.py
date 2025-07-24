01189f2ff0ae0f4830bcf20ebadd746b47db3cea
Daniel Lam <dltlam1987@gmail.com>
2025-07-24 02:18:42 -0700

    Time: 7 ms (69.33%), Space: 17.9 MB (69.2%) - LeetHub

--- 0636-exclusive-time-of-functions/0636-exclusive-time-of-functions.py
+++ 0636-exclusive-time-of-functions/0636-exclusive-time-of-functions.py
@@ -1,25 +1,26 @@
 class Solution:
     def exclusiveTime(self, n: int, logs: List[str]) -> List[int]:
-        stack = [] # Stack to store the IDs of functions currently running
-        res = [0] * n # Result array to store exclusive time at function i
-        prev = 0
+        stack = []                # Stack to store the IDs of currently running functions
+        res = [0] * n             # Result array to store exclusive time for each function
+        prev_time = 0             # Tracks the time of the last event
 
         for i in range(len(logs)):
-            fn_id, typ, timestamp = logs[i].split(":") # Parse the log
-            fn_id = int(fn_id) # Convert function ID to int
-            current_time = int(timestamp) # Convert timetsamp to int
+            fn_id, typ, timestamp = logs[i].split(":")   # Parse the log entry
+            fn_id = int(fn_id)                           # Convert function ID to int
+            current_time = int(timestamp)                # Convert timestamp to int
 
             if typ == "start":
                 if stack:
-                    # Add time since last update to the function currently running
+                    # Add elapsed time to the function on top of the stack
                     res[stack[-1]] += current_time - prev_time
-                stack.append(fn_id)
-                prev_time = current_time
+                stack.append(fn_id)                      # Start new function
+                prev_time = current_time                 # Update prev_time
             else:
-                 # Current function ends now; include this timestamp (+1)
-                 res[stack.pop()] += current_time - prev_time + 1
-                 prev_time = current_time + 1
-        return res
+                # End current function and include the current_time (inclusive)
+                res[stack.pop()] += current_time - prev_time + 1
+                prev_time = current_time + 1             # Resume next function after this
+
+        return res                                        # Return the exclusive time per function
 
 
 
