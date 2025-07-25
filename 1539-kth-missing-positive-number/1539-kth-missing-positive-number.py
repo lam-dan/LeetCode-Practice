00f8a78674c79a6f9b816eef47b00f2cab85f1a7
Daniel Lam <dltlam1987@gmail.com>
2025-07-25 04:20:47 -0700

    Time: 0 ms (100%), Space: 18.1 MB (15.71%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.py
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.py
@@ -10,17 +10,11 @@ class Solution:
         #  0 1 2 3 4
         # [2,3,4,7,11]
         # [1,1,1,3,6]
-
-        # missing_numbers = [0] * len(arr)
-        # for i in range(len(arr)):
-        #     missing_numbers[i] = arr[i] - i - 1
-
         left = 0 
         right = len(arr) - 1
 
-        # print("missing", missing_numbers)
-
         while left <= right:
+
             mid = (left + right) // 2
             missing = arr[mid] - (mid + 1)
 
@@ -28,8 +22,7 @@ class Solution:
                 left = mid + 1
             else:
                 right = mid - 1
-        
-        #  arr[right] + k - (arr[right] - right - 1)
+                
         return left + k
 
 
