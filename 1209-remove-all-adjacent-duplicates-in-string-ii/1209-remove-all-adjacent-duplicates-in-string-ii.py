3b3eaf204d79335bc2e2f4b4518db5a16736b65d
Daniel Lam <dltlam1987@gmail.com>
2025-07-30 04:21:04 -0700

    Time: 50 ms (36.98%), Space: 21.7 MB (79.21%) - LeetHub

--- 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.py
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.py
@@ -23,12 +23,9 @@ class Solution:
         - In the worst case (no removals), the stack stores one entry per character.
         """
         stack = [] # Stack to track (character, count) tuples
-
         # Traverse input string character by character
         for i in range(len(s)):
-            char = s[i]
-
-            if stack and stack[-1][0] == char:
+            if stack and stack[-1][0] == s[i]:
                 # Same as previous character
                 prev_char, prev_count = stack.pop()
                 new_count = prev_count + 1
@@ -36,11 +33,14 @@ class Solution:
                 if new_count == k:
                     continue # Reached k duplicates, so we skip pushing, this removes the entire group
                 if new_count < k:
-                    stack.append((char, new_count)) # push back into stack since it's not at k yet
+                    stack.append((s[i], new_count)) # push back into stack since it's not at k yet
             else:
-                stack.append((char, 1))
+                stack.append((s[i], 1)) # pushing current char into stack with count of 1
         
-        return ''.join(char * count for (char, count) in stack)
+        return ''.join(char * count for (char, count) in stack) # Rebuild string from tuple containing char and count
+
+        # Time Complexity is O(n) where n is the number of characters in the string s
+        # Space Complexity is O(n) where n is the number of characters in the string s
 
                 
 
