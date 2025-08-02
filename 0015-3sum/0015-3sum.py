534effcb1ac737528bd91de3c5369c2888da3194
Daniel Lam <dltlam1987@gmail.com>
2025-08-02 06:10:50 -0700

    Time: 595 ms (44.68%), Space: 20.8 MB (36.38%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,30 +1,27 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        res = []
         nums.sort()
+        res = []
+
         for i in range(len(nums)):
-            if nums[i] > 0:
-                break
-            if i == 0 or nums[i - 1] != nums[i]:
-                self.two_pointers(nums, i, res)
-        return res
-
-    def two_pointers(self, nums: List[int], i: int, res):
-        left = i + 1
-        right = len(nums) - 1
-        while left < right:
-            total = nums[i] + nums[left] + nums[right]
-            if total == 0:
-                res.append([nums[i], nums[left], nums[right]])
-                left += 1
-                right -= 1
-                while left < right and nums[left - 1] == nums[left]:
-                    left += 1
-            elif total < 0:
-                left += 1
-            elif total > 0:
-                right -= 1
-        
-
-
+            # if nums[i] > 0:
+            #     break
             
+            if i == 0 or nums[i - 1] != nums[i]:
+                # Two Pointer
+                left = i + 1
+                right = len(nums) - 1
+
+                while left < right:
+                    total = nums[i] + nums[left] + nums[right]
+                    if total == 0:
+                        res.append([nums[i], nums[left], nums[right]])
+                        left += 1
+                        right -= 1
+                        while left < right and nums[left - 1] == nums[left]:
+                            left += 1
+                    elif total > 0:
+                        right -= 1
+                    else:  
+                        left += 1
+        return res
