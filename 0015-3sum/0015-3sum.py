fea9d886b09ac0eddc087b4c0722456d2b55ab3a
Daniel Lam <dltlam1987@gmail.com>
2025-08-02 05:42:13 -0700

    Time: 470 ms (84.13%), Space: 20.6 MB (69.39%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,25 +1,32 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        result = []
         nums.sort()
+        res = []
 
         for i in range(len(nums)):
-            if i > 0 and nums[i] == nums[i - 1]:
-                continue
-            left, right = i + 1, len(nums) - 1
-            while left < right:
-                three_sum = nums[i] + nums[left] + nums[right]
-                if three_sum == 0:
-                    result.append([nums[i], nums[left], nums[right]])
+            if nums[i] > 0:
+                break
+            if i == 0 or nums[i - 1] != nums[i]:
+                self.two_pointers(nums, i, res)
+        return res
+
+    def two_pointers(self, nums: List[int], i: int, res: List[List[int]]):
+        left = i + 1
+        right = len(nums) - 1
+
+        while left < right:
+            total = nums[i] + nums[left] + nums[right]
+            if total == 0:
+                res.append([nums[i], nums[left], nums[right]])
+                left += 1
+                right -= 1
+                while left < right and nums[left] == nums[left - 1]:
                     left += 1
-                    right -= 1
-                    while left < right and nums[left] == nums[left - 1]:
-                        left += 1
-                    while left < right and nums[right] == nums[right + 1]:
-                        right -= 1
-                elif three_sum > 0:
-                    right -= 1
-                else:
-                    left += 1
-        return result
-                
+            elif total < 0:
+                left += 1
+            elif total > 0:
+                right -= 1
+        
+
+
+            
