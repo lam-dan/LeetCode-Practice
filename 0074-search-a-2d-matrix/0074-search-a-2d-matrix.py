7e6c54db339879a5ac645fbf6993644cbd16b9b4
Daniel Lam <dltlam1987@gmail.com>
2025-08-01 17:36:00 -0700

    Time: 0 ms (100%), Space: 18.3 MB (25.73%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -10,6 +10,9 @@ class Solution:
         left = 0
         right = len(array) - 1
 
+        # Problem statement implies left most insertion point, but in this case,
+        # left <= right	ensures the search space is fully exhausted and the last candidate index is processed
+        # left < right	May exit early, skipping valid candidate rows
         while left <= right:
             mid = (left + right) // 2
             if array[mid][0] == target:
@@ -18,7 +21,24 @@ class Solution:
                 right = mid - 1
             else:
                 left = mid + 1
-        return left - 1
+        return left - 1 # Handles edge case where left is basically
+        # matrix = [
+        #     [1, 2, 3],    # Row 0
+        #     [5, 6, 7],    # Row 1
+        #     [9, 10, 11]   # Row 2
+        # ]
+
+        # 1. Target Lies Between Two Rows (Standard Floor Search Case)
+        # Target = 7
+        # returns left = 2 incorrectly
+
+        # 2. Target Smaller than All Rows (Overshoot Below)
+        # Target = 12
+        # returns left = 2 incorrectly
+
+        # 3. Target Larger than All Rows (Overshoot Above)
+        # Target = 12
+        # returns left = 2 incorrectly
 
     def binary_search_target(self, array:List, target:int):
         left = 0
