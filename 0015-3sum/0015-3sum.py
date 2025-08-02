b1c184dbb9ab8a66b09d6ced75bab251a0e734ca
Daniel Lam <dltlam1987@gmail.com>
2025-08-02 06:11:39 -0700

    Time: 614 ms (35.64%), Space: 20.6 MB (69.39%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -4,24 +4,24 @@ class Solution:
         res = []
 
         for i in range(len(nums)):
-            # if nums[i] > 0:
-            #     break
-            
-            if i == 0 or nums[i - 1] != nums[i]:
-                # Two Pointer
-                left = i + 1
-                right = len(nums) - 1
+            if i > 0 and nums[i - 1] == nums[i]:
+                continue
+            # Two Pointer
+            left = i + 1
+            right = len(nums) - 1
 
-                while left < right:
-                    total = nums[i] + nums[left] + nums[right]
-                    if total == 0:
-                        res.append([nums[i], nums[left], nums[right]])
+            while left < right:
+                total = nums[i] + nums[left] + nums[right]
+                if total == 0:
+                    res.append([nums[i], nums[left], nums[right]])
+                    left += 1
+                    right -= 1
+                    while left < right and nums[left] == nums[left -1]:
                         left += 1
+                    while left < right and nums[right] == nums[right + 1]:
                         right -= 1
-                        while left < right and nums[left - 1] == nums[left]:
-                            left += 1
-                    elif total > 0:
-                        right -= 1
-                    else:  
-                        left += 1
+                elif total > 0:
+                    right -= 1
+                else:  
+                    left += 1
         return res
