ad9680f1b75e7d6383ed04859ad176d57940d799
Daniel Lam <dltlam1987@gmail.com>
2025-08-04 01:49:35 -0700

    Time: 0 ms (100%), Space: 18.6 MB (37.23%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -5,29 +5,43 @@ class Solution:
         """
         rows = len(matrix)
         cols = len(matrix[0])
-        dir = [(-1,0), (1,0), (0,1), (0, -1)]
+        first_col_has_zero = False
 
-        print('matrix_bef', matrix)
+        # Iterate through a the "INNER" 2D matrix excluding the first column and row
+        # when a cell is detected as a 0, mark the corresponding row and col 0s
+        for r in range(rows):
+            # If any value in the first column is 0, set the flag to true to do some processing later
+            if matrix[r][0] == 0:
+                first_col_has_zero = True
+            for c in range(1, cols): # Iterate through inner cells
+                if matrix[r][c] == 0: # If a zero is detected in the cell
+                    matrix[r][0] = 0 # Mark the outer row as 0, the row in the first column
+                    matrix[0][c] = 0 # Mark the outer col as 0, the col in the first row
 
-        for r in range(rows):
-            for c in range(cols):
-                if matrix[r][c] == 0:
-                    for i in range(rows):
-                        if matrix[i][c] != 0:
-                            matrix[i][c] = '#'
-                    for i in range(cols):
-                        if matrix[r][i] != 0:
-                            matrix[r][i] = '#' 
-        print('matrix 1', matrix)
-        for r in range(rows):
-            for c in range(cols):
-                if matrix[r][c] == "#":
+        # Iterate through the "INNER" 2D matrix again and zero out all cells in the rows and cols 
+        # that were marked earlier
+        for r in range(1, rows):
+            for c in range(1, cols):
+                if matrix[r][0] == 0 or matrix[0][c] == 0:
                     matrix[r][c] = 0
-        print('matrix 2', matrix)
+        
+        # Handle the first row now since you don't want to handle them earlier as they
+        # might be overrided through the loop right before
+        if matrix[0][0] == 0:
+            for c in range(cols):
+                matrix[0][c] = 0
+        
+        # Handle the first col last to make sure they are zeroed out as well
+        if first_col_has_zero:
+            for r in range(rows):
+                matrix[r][0] = 0
+
         return matrix
 
 
 
-                        
 
 
+
+        
+        
