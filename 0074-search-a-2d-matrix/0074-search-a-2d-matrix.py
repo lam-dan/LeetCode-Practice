4b50a519a104e0ccd4a23cb48e36323fe1d0e403
Daniel Lam <dltlam1987@gmail.com>
2025-08-03 23:25:12 -0700

    Time: 0 ms (100%), Space: 18.2 MB (71.65%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -1,18 +1,13 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        if len(matrix) == 0:
-            return False
         index = self.binary_search_index(matrix, target)
-        array_with_num = self.binary_search_target(matrix[index], target)
-        return  array_with_num
+        result = self.binary_search_target(matrix[index], target)
+        return result
 
-    def binary_search_index(self, array:List, target:int):
+    def binary_search_index(self, array, target):
         left = 0
         right = len(array) - 1
 
-        # Problem statement implies left most insertion point, but in this case,
-        # left <= right	ensures the search space is fully exhausted and the last candidate index is processed
-        # left < right	May exit early, skipping valid candidate rows
         while left <= right:
             mid = (left + right) // 2
             if array[mid][0] == target:
@@ -21,31 +16,9 @@ class Solution:
                 right = mid - 1
             else:
                 left = mid + 1
-        return max(0, right) # Handles edge case where left is basically
-        # You can also return the right pointer
+        return right
 
-        # matrix = [
-        #     [1, 2, 3],    # Row 0
-        #     [5, 6, 7],    # Row 1
-        #     [9, 10, 11]   # Row 2
-        # ]
-
-        # 1. Target Lies Between Two Rows (Standard Floor Search Case)
-        # Target = 7
-        # returns left = 2 incorrectly
-        # return right = 1 Correctly
-
-        # 2. Target Smaller than All Rows (Overshoot Below)
-        # Target = 12
-        # returns left = 0 incorrectly
-        # return right = -1 Correctly -> Leads to False
-
-        # 3. Target Larger than All Rows (Overshoot Above)
-        # Target = 12
-        # returns left = 3 incorrectly
-        # returns right = 2 correctly
-
-    def binary_search_target(self, array:List, target:int):
+    def binary_search_target(self, array, target):
         left = 0
         right = len(array) - 1
 
