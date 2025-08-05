545d6d2bf3889f5b27e4f77ad685242576bcbcbe
Daniel Lam <dltlam1987@gmail.com>
2025-08-04 18:19:05 -0700

    Time: 0 ms (100%), Space: 18.3 MB (25.58%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -1,13 +1,18 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        if len(matrix) == 0:
+            return False
         index = self.binary_search_index(matrix, target)
-        result = self.binary_search_target(matrix[index], target)
-        return result
+        array_with_num = self.binary_search_target(matrix[index], target)
+        return  array_with_num
 
-    def binary_search_index(self, array, target):
+    def binary_search_index(self, array:List, target:int):
         left = 0
         right = len(array) - 1
 
+        # Problem statement implies left most insertion point, but in this case,
+        # left <= right	ensures the search space is fully exhausted and the last candidate index is processed
+        # left < right	May exit early, skipping valid candidate rows
         while left <= right:
             mid = (left + right) // 2
             if array[mid][0] == target:
@@ -16,9 +21,26 @@ class Solution:
                 right = mid - 1
             else:
                 left = mid + 1
-        return right
+        return left - 1 # Handles edge case where left is basically
+        # matrix = [
+        #     [1, 2, 3],    # Row 0
+        #     [5, 6, 7],    # Row 1
+        #     [9, 10, 11]   # Row 2
+        # ]
 
-    def binary_search_target(self, array, target):
+        # 1. Target Lies Between Two Rows (Standard Floor Search Case)
+        # Target = 7
+        # returns left = 2 incorrectly
+
+        # 2. Target Smaller than All Rows (Overshoot Below)
+        # Target = 12
+        # returns left = 2 incorrectly
+
+        # 3. Target Larger than All Rows (Overshoot Above)
+        # Target = 12
+        # returns left = 2 incorrectly
+
+    def binary_search_target(self, array:List, target:int):
         left = 0
         right = len(array) - 1
 
