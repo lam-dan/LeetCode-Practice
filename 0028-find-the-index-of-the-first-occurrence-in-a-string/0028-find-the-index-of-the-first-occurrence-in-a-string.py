538760909c33317fd82feff57000f5a20a358018
Daniel Lam <dltlam1987@gmail.com>
2025-08-07 03:38:06 -0700

    Time: 0 ms (100%), Space: 17.6 MB (96.8%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
@@ -1,27 +1,41 @@
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
-        if len(needle) > len(haystack):
-            return -1
+        # Knutt Morris KMP Algorithm for O(n) time complexity where n is the number of 
+        # elements in haystack
+        # However, the trade off is O(m) space where m is the longer array of haystack or needle   
 
-        needle_count = 0
-        index = None
+        lps = self.build_lps(needle)
+        print("lps", lps)
+        i = 0
+        j = 0
 
-        for i in range(len(haystack)):
-            j = 0
-            while i < len(haystack) and haystack[i] == needle[j]:
-                if j == 0:
-                    index = i
-                needle_count += 1
+        while i < len(haystack):
+            if haystack[i] == needle[j]:
                 i += 1
                 j += 1
-                if needle_count == len(needle):
-                    return index
-            needle_count = 0
+                if j == len(needle):
+                    return i - j
+            else:
+                if j > 0:
+                    j = lps[j - 1]
+                else:
+                    i += 1
         return -1
 
+    def build_lps(self, needle):
+        lps = [0] * len(needle)
+        length = 0
 
-
-
-                
-            
-
+        i = 1
+        while i < len(needle):
+            if needle[i] == needle[length]:
+                length += 1
+                lps[i] = length
+                i += 1
+            else:
+                if length > 0:
+                    length = lps[length - 1]
+                else:
+                    lps[i] = 0
+                    i += 1
+        return lps
