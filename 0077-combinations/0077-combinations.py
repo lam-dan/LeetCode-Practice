54c840faaadcab9aa315a90b1156901fcef7f56a
Daniel Lam <dltlam1987@gmail.com>
2025-08-07 05:00:34 -0700

    Time: 93 ms (93.64%), Space: 59.7 MB (55.54%) - LeetHub

--- 0077-combinations/0077-combinations.py
+++ 0077-combinations/0077-combinations.py
@@ -1,25 +1,6 @@
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
-        # Classic BackTracking Problem
-        # 1. We need to explore all valid combinations
-        # You're tasked with finding all combinations of k numbers from [1, n]. 
-        # That means we need to systematically explore all possible choices of numbers, but:
-        # Not allow duplicates (e.g. [1, 2] and [2, 1] are the same → pick numbers in increasing order)
-        # Not go beyond size k
-        # Explore one choice, and then back up to try the next one
-        # This is exactly what backtracking is built for:
-
-        # 2. Backtracking builds a combinatorial tree
-        # Each level of recursion represents picking the next number:
-        # At level 0: pick any number from 1 to n-k+1
-        # At level 1: pick from the next available numbers
-        # Until we hit size k, then we backtrack and try another path
-        # This tree of decisions is a natural fit for recursion + backtracking.
-
-        # 3. Backtracking = Efficient + Clean
-        # You avoid unnecessary work by pruning early (don’t explore paths longer than k)
-        # You don't need to generate all subsets and then filter — you only build valid ones
-        # You get compact code that handles all branching cases
+        
         self.results = []
 
         def dfs(array, first_num):
@@ -28,14 +9,11 @@ class Solution:
                 self.results.append(array.copy())
                 return
 
-            # Pruning Optimization
-            max_start = n - (k - len(array)) + 1
-
-            for num in range(first_num, max_start + 1):
-                array.append(num)
-                dfs(array, num + 1)
+            last_num = n - (k - len(array)) + 1
+            
+            for i in range(first_num, last_num + 1):
+                array.append(i)
+                dfs(array, i + 1)
                 array.pop()
         dfs([], 1)
         return self.results
-
-
