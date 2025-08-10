cfcd411b4ec1da9ba6eab9aefa45386b0962cf58
Daniel Lam <dltlam1987@gmail.com>
2025-08-10 03:40:58 -0700

    Time: 55 ms (90.86%), Space: 17.9 MB (36.35%) - LeetHub

--- 0018-4sum/0018-4sum.py
+++ 0018-4sum/0018-4sum.py
@@ -2,7 +2,7 @@ class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
         n = len(nums)
         nums.sort()
-        res: List[List[int]] = []
+        res = []
 
         # Early exit
         if n < 4:
@@ -23,35 +23,37 @@ class Solution:
 
             for j in range(i + 1, n - 2):
                 # Skip duplicate b's (with the same i)
-                if j > i + 1 and nums[j] == nums[j - 1]:
-                    continue
+                if j == i + 1 or nums[j] != nums[j - 1]:
+                    
 
-                # Optional pruning for the j loop
-                min_sum_j = nums[i] + nums[j] + nums[j + 1] + nums[j + 2]
-                if min_sum_j > target:
-                    break  # increasing j will only increase sums here
-                max_sum_j = nums[i] + nums[j] + nums[n - 1] + nums[n - 2]
-                if max_sum_j < target:
-                    continue
+                # # Optional pruning for the j loop
+                # min_sum_j = nums[i] + nums[j] + nums[j + 1] + nums[j + 2]
+                # if min_sum_j > target:
+                #     break  # increasing j will only increase sums here
+                # max_sum_j = nums[i] + nums[j] + nums[n - 1] + nums[n - 2]
+                # if max_sum_j < target:
+                #     continue
 
                 # Two pointers for c, d
-                left, right = j + 1, n - 1
-                while left < right:
-                    total = nums[i] + nums[j] + nums[left] + nums[right]
+                    left = j + 1
+                    right = n - 1
 
-                    if total == target:
-                        res.append([nums[i], nums[j], nums[left], nums[right]])
-                        # Move left/right past duplicates
-                        left += 1
-                        while left < right and nums[left] == nums[left - 1]:
+                    while left < right:
+                        total = nums[i] + nums[j] + nums[left] + nums[right]
+
+                        if total == target:
+                            res.append([nums[i], nums[j], nums[left], nums[right]])
+                            # Move left/right past duplicates
                             left += 1
-                        right -= 1
-                        while left < right and nums[right] == nums[right + 1]:
+                            while left < right and nums[left] == nums[left - 1]:
+                                left += 1
                             right -= 1
-                    elif total < target:
-                        left += 1
-                    else:
-                        right -= 1
+                            while left < right and nums[right] == nums[right + 1]:
+                                right -= 1
+                        elif total < target:
+                            left += 1
+                        else:
+                            right -= 1
 
         return res
         
