069e3b5cfe35a1f0355adad5bad72159424633d8
Daniel Lam <dltlam1987@gmail.com>
2025-08-14 03:49:28 -0700

    Time: 5 ms (60.82%), Space: 20.7 MB (95.9%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.py
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.py
@@ -6,21 +6,19 @@
 #         self.right = right
 class Solution:
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
-        # We need to track 3 things
-        # left subtree depth
-        # right subtree depth
-        # maximum diameter
-        self.max_diameter = 0
+        # DFS Approach  
+
+        self.diameter = 0
 
         def dfs(node):
             if node is None:
                 return 0
-            left_depth = dfs(node.left)
-            right_depth = dfs(node.right)
-            # Update global max diameter
-            self.max_diameter = max(self.max_diameter, left_depth + right_depth)
-            # Plus 1 is to count the current node on the return
-            # max is to converge the counts at each root of the subtree
-            return 1 + max(left_depth, right_depth)
+            node_left = dfs(node.left)
+            node_right = dfs(node.right)
+
+            self.diameter = max(self.diameter, node_left + node_right)
+
+            return 1 + max(node_left, node_right)
+
         dfs(root)
-        return self.max_diameter
+        return self.diameter
