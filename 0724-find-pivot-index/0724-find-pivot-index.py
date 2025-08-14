3cd56411e1fa1761027e68d97ed04ab9ab327bc9
Daniel Lam <dltlam1987@gmail.com>
2025-08-14 11:58:56 -0700

    Time: 7 ms (57.22%), Space: 18.7 MB (53.97%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.py
+++ 0724-find-pivot-index/0724-find-pivot-index.py
@@ -1,19 +1,11 @@
 class Solution:
     def pivotIndex(self, nums: List[int]) -> int:
-        total_sum = sum(nums)
-        left_sum = 0
+        total = sum(nums)
 
-        # At each point in time we have the running total of the left sum 
-        # we check to see if that left sum is equal to the right side by the formula
-        # total sum of the nums array subtracted by the left sum and the current value of the pointer
-        # this will give us the sum on the right hand side.
+        running_total = 0
         for i in range(len(nums)):
-            if left_sum == (total_sum - left_sum - nums[i]):
+            running_total += nums[i - 1] if i > 0 else 0
+            if running_total == (total - running_total - nums[i]):
                 return i
-            left_sum += nums[i] # If they're not equal, continue adding current value to perfix sum
         return -1
 
-        # Time Complexity is O(n)
-        # Space Complexity is O(1)
-
-        
