3cd665717130b6b65f29a2e98fa7f20317f96193
Daniel Lam <dltlam1987@gmail.com>
2025-08-14 03:25:40 -0700

    Time: 0 ms (100%), Space: 17.8 MB (50.64%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        mapping = {
+            ")": "(",
+            "}": "{",
+            "]": "["
+            }
+
+        for i in range(len(s)):
+            if s[i] in mapping:
+                top_element = stack.pop() if stack else "#"
+
+                if mapping[s[i]] != top_element:
+                    return False
+            else:
+                stack.append(s[i])
+        
+        if len(stack) > 0:
+            return False
+        return True
