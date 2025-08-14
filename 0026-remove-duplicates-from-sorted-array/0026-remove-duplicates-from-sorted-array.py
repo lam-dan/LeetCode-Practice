50dbd6c410227b1b999e7ddb320fc33be157b33e
Daniel Lam <dltlam1987@gmail.com>
2025-08-14 10:43:25 -0700

    Time: 0 ms (100%), Space: 18.8 MB (74.37%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -1,23 +1,13 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        # 2 pointers
-        # i to keep track of position of iteration
-        # j to updates in the array
         i = 1
         j = 1
+
+
         while i < len(nums):
             if nums[i] != nums[i - 1]:
                 nums[j] = nums[i]
                 j += 1
             i += 1
-        return j # Return the index of our last update
-
-        # Time Complexity is O(n) - worst case we iterate through n number of numbers
-        # Space Complexity is O(1) - pointers only
-
-        
-
-            
-
-
+        return j
         
