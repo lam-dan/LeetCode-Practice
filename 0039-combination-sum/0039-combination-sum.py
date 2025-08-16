bfeb95fcac99a6db5b9e33542ce25ad53fa4a263
Daniel Lam <dltlam1987@gmail.com>
2025-08-16 02:36:41 -0700

    Time: 14 ms (29.83%), Space: 17.8 MB (61.43%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -9,18 +9,20 @@ class Solution:
         # we want to copy the arry once we hit our target
         # Iterate through all indexes 
         self.result = []
-        def dfs(i, total):
+
+        def dfs(i, total, running_total):
             # Base Case
-            if i >= len(candidates) or sum(total) > target:
+            if i >= len(candidates) or running_total > target:
                 return 
-            if sum(total) == target:
+
+            if running_total == target:
                 self.result.append(total.copy())
                 return
 
             total.append(candidates[i])
-            dfs(i, total)
+            dfs(i, total, running_total + candidates[i])
             total.pop()
-            dfs(i + 1, total)
-        dfs(0, [])
+            dfs(i + 1, total, running_total)
 
+        dfs(0, [], 0)
         return self.result
