1108ee8ffcf8ac371d87acad120e744476a9bd0a
Daniel Lam <dltlam1987@gmail.com>
2025-08-22 02:09:59 -0700

    Time: 0 ms (100%), Space: 19.1 MB (74.37%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.py
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.py
@@ -5,31 +5,17 @@
 #         self.next = next
 class Solution:
     def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        # If the linked list is empty, return None immediately
         if head is None:
             return None
 
-        # Initialize two pointers:
-        # odd -> points to the first node (odd index)
-        # even -> points to the second node (even index)
         odd = head
-        even = odd.next
-        
-        # Save the head of the even list so we can append it later
+        even = head.next
         evenhead = even
 
-        # Loop until there are no more even nodes or no more nodes after an even node
         while even and even.next:
-            # Link the current odd node to the next odd node (skip the even node)
             odd.next = even.next
-            odd = odd.next  # Move odd pointer forward
-
-            # Link the current even node to the next even node (skip the odd node)
+            odd = odd.next
             even.next = odd.next
-            even = even.next  # Move even pointer forward
-
-        # Append the even list after the odd list
+            even = even.next
         odd.next = evenhead
-
-        # Return the head of the rearranged linked list
         return head
