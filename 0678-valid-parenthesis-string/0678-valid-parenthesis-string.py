9d9fb0ee425e052ca0c03d604160a7fdb7640243
Daniel Lam <dltlam1987@gmail.com>
2025-08-28 23:04:15 -0700

    Time: 0 ms (100%), Space: 17.7 MB (57.36%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.py
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.py
@@ -3,31 +3,27 @@ class Solution:
         open_stack = []
         star_stack = []
 
-        # Pass 1: Scan from Left to Right
         for i in range(len(s)):
             if s[i] == "(":
                 open_stack.append(i)
             elif s[i] == "*":
                 star_stack.append(i)
             elif s[i] == ")":
-                # Removal process from stacks
+                if len(open_stack) == 0 and len(star_stack) == 0:
+                    return False
+                    
                 if len(open_stack) > 0:
                     open_stack.pop()
                 elif len(star_stack) > 0:
                     star_stack.pop()
-                else:
-                    return False
-            else:
+
+        while len(open_stack) > 0 and len(star_stack) > 0:
+            if open_stack.pop() > star_stack.pop():
                 return False
 
-        # # Pass 2: Scan from Right to Left
-        while len(open_stack) > 0 and len(star_stack) > 0:
-            if open_stack[-1] < star_stack[-1]:
-                open_stack.pop()
-                star_stack.pop()
-            else:
-                return False
-        return len(open_stack) == 0
+        if len(open_stack) > 0:
+            return False
+        return True
         
 
 
