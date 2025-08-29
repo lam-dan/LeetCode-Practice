62ec5d994a253d58107e358a92f6a64eb2738b42
Daniel Lam <dltlam1987@gmail.com>
2025-08-29 05:11:36 -0700

    Time: 3 ms (19.22%), Space: 17.8 MB (40.73%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.py
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.py
@@ -1,30 +1,29 @@
 class Solution:
     def checkValidString(self, s: str) -> bool:
         open_stack = []
-        star_stack = []
+        close_stack = []
 
         for i in range(len(s)):
-            if s[i] == "(":
-                open_stack.append(i)
-            elif s[i] == "*":
-                star_stack.append(i)
-            elif s[i] == ")":
-                if len(open_stack) == 0 and len(star_stack) == 0:
-                    return False
-                    
-                if len(open_stack) > 0:
-                    open_stack.pop()
-                elif len(star_stack) > 0:
-                    star_stack.pop()
-
-        if len(open_stack) > 0 and len(star_stack) > 0:
-            if open_stack.pop() > star_stack.pop():
+            if s[i] == ")":
+                close_stack.append(s[i])
+            else:
+                open_stack.append(s[i])
+            if len(close_stack) > len(open_stack):
                 return False
 
-        if len(open_stack) > 0:
-            return False
+        open_stack = []
+        close_stack = []
+
+
+        for i in range(len(s)-1, -1, -1):
+            if s[i] == "(":
+                open_stack.append(s[i])
+            else:
+                close_stack.append(s[i])
+            if len(open_stack) > len(close_stack):
+                return False
         return True
-        
 
 
-        
+
+            
