4b6217ee4fc4721af0452086a5a24f748fdab65c
Daniel Lam <dltlam1987@gmail.com>
2025-09-06 04:53:47 -0700

    Time: 10 ms (66.46%), Space: 18.1 MB (29.42%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.py
+++ 0006-zigzag-conversion/0006-zigzag-conversion.py
@@ -1,31 +1,22 @@
 class Solution:
     def convert(self, s: str, numRows: int) -> str:
-        # https://youtu.be/Q2Tw6gcVEwc?t=116
-        if numRows == 1 or numRows >= len(s):
+
+        if numRows == 1 or numRows > len(s) - 1:
             return s
+
         result = []
-        step = 2 * (numRows - 1) # one full zigzag section
-        # step represents the number of characters you pass through to get to the same row position
+        # number of steps to get to next character in the 2D matrix
+        steps = 2 * (numRows - 1)
+
         for r in range(numRows): # 0, 1, 2
-            i = r # i = 0, !1, 2
-                                        #0 1 2 3 !4 5 6 7 8 9 10 11 12 13
-            while i < len(s): #  n = len(!P !A Y !P !A L I S !H I R   I  !N G)
+            i = r
+
+            while i < len(s): 
                 result.append(s[i])
-                # if middle rows also have a diagonal hit inside the same section
-                if 0 < r < numRows - 1:
-                    # 1 + (4 - 2 * 1)
-                    # 1 + 2 = 3
-                    j = i + (step - 2 * r) # find the index of the diagonal based on the step
-                    # 3 < 
-                    if j < len(s): #if we're still in the boundaries of the string
+
+                if 0 < r < numRows - 1: # Within the boundaries of the middle index of the matrix
+                    j = i + (steps - 2 * r)
+                    if j < len(s):
                         result.append(s[j])
-                i += step # jump to next section (same row) 0, 4
-                # [P A H N A P]
+                i += steps
         return "".join(result)
-
-        # O(n)
-        # O(1)
-
-
-
-
