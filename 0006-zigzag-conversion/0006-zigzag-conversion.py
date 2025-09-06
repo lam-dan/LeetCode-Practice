536b7f384d8c67a3a8942bd6bc01cf947e6e6983
Daniel Lam <dltlam1987@gmail.com>
2025-09-06 03:50:09 -0700

    Time: 7 ms (87.98%), Space: 17.8 MB (89.95%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.py
+++ 0006-zigzag-conversion/0006-zigzag-conversion.py
@@ -1,27 +1,30 @@
 class Solution:
     def convert(self, s: str, numRows: int) -> str:
-        n = len(s)
-        if numRows == 1 or numRows >= n:
+        # https://youtu.be/Q2Tw6gcVEwc?t=116
+        if numRows == 1 or numRows >= len(s):
             return s
-
-        out = []
-        step = 2 * (numRows - 1)
-
-
-        print("step", step)
-        
-        for r in range(numRows):
-            i = r
-            while i < n:
-                out.append(s[i])
-
+        result = []
+        step = 2 * (numRows - 1) # one full zigzag section
+        # step represents the number of characters you pass through to get to the same row position
+        for r in range(numRows): # 0, 1, 2
+            i = r # i = 0, !1, 2
+                                        #0 1 2 3 !4 5 6 7 8 9 10 11 12 13
+            while i < len(s): #  n = len(!P !A Y !P !A L I S !H I R   I  !N G)
+                result.append(s[i])
+                # if middle rows also have a diagonal hit inside the same section
                 if 0 < r < numRows - 1:
-                    j = i + (step - 2*r)
-                    if j < n:
-                        out.append(s[j])
-                
-                i += step
-        return "".join(out)
+                    # 1 + (4 - 2 * 1)
+                    # 1 + 2 = 3
+                    j = i + (step - 2 * r) # find the index of the diagonal based on the step
+                    # 3 < 
+                    if j < len(s): #if we're still in the boundaries of the string
+                        result.append(s[j])
+                i += step # jump to next section (same row) 0, 4
+                # [P A H N A P]
+        return "".join(result)
+
+        # O(n)
+        # O(1)
 
 
 
