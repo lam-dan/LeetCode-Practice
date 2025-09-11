f451f225cc1049173477def69250900c5e010e1f
Daniel Lam <dltlam1987@gmail.com>
2025-09-11 02:08:11 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,18 +1,14 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        # Binary search in rotated sorted array
         left = 0
-        right = len(nums) - 1 
-
+        right = len(nums) - 1
         while left <= right:
             mid = (left + right) // 2
-            print("left", left)
-            print("right", right)
-            print("mid", mid)
+
             if nums[mid] == target:
                 return mid
-            
-            if nums[left] <= nums[mid]: # Search is left rotated sorted array
+
+            if nums[left] <= nums[mid]:
                 if nums[left] <= target <= nums[mid]:
                     right = mid - 1
                 else:
@@ -21,8 +17,5 @@ class Solution:
                 if nums[mid] <= target <= nums[right]:
                     left = mid + 1
                 else:
-                    right = mid - 1
+                    right = mid + 1
         return -1
-            
-
-
