ec6bbca3b111437fa497bdec68078e64053e9f1e
Daniel Lam <dltlam1987@gmail.com>
2025-09-11 02:02:54 -0700

    Time: 0 ms (100%), Space: 18.2 MB (42.09%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,37 +1,28 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
+        # Binary search in rotated sorted array
         left = 0
-        right = len(nums) - 1
+        right = len(nums) - 1 
 
-        # Standard binary search loop
         while left <= right:
             mid = (left + right) // 2
-
-            # Base case: found the target
+            print("left", left)
+            print("right", right)
+            print("mid", mid)
             if nums[mid] == target:
                 return mid
-
-            # Determine which half is sorted
-            # If left pointer is less than right pointer
-            # then Left half is sorted
-            if nums[left] <= nums[mid]: 
-                # Check if target is within the left sorted range
-                if nums[left] <= target < nums[mid]:
-                    # Target is in the left half — discard right half
+            
+            if nums[left] <= nums[mid]: # Search is left rotated sorted array
+                if nums[left] <= target <= nums[mid]:
                     right = mid - 1
                 else:
-                    # Target is not in the left half — search right half
                     left = mid + 1
-            else: # Right half is sorted
-                # Check if target is within the right sorted range
-                if nums[mid] < target <= nums[right]:
-                    # Target is in the right half — discard left half
+            else:
+                if nums[mid] <= target <= nums[right]:
                     left = mid + 1
                 else:
-                    # Target is not in the right half — search left half
                     right = mid - 1
-        # Target not found in the array
         return -1
-
+            
 
 
