da68c2ffc3e9c2b11a315d24d508da2bb26f4519
Daniel Lam <dltlam1987@gmail.com>
2025-09-16 15:01:17 -0700

    Time: 240 ms (70.56%), Space: 20.2 MB (57.97%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -2,30 +2,30 @@ class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         rows = len(grid)
         cols = len(grid[0])
-        count = 0
 
-        def dfs(i,j):
+        def dfs(r,c):
+            # Boundary Checks
             if (
-                i < 0 or
-                j < 0 or
-                i >= rows or
-                j >= cols or
-                grid[i][j] == "0"
+                r < 0 or
+                r >= rows or
+                c < 0 or 
+                c >= cols or
+                grid[r][c] == "0"
             ):
                 return
 
-            grid[i][j] = "0"
-
-            dfs(i + 1, j)
-            dfs(i - 1, j)
-            dfs(i, j + 1)
-            dfs(i, j - 1)
-
+            grid[r][c] = "0"
+            
+            dfs(r+1, c)
+            dfs(r-1, c)
+            dfs(r, c + 1)
+            dfs(r, c - 1)
             return
 
-        for i in range(rows):
-            for j in range(cols):
-                if grid[i][j] == "1":
-                    dfs(i,j)
+        count = 0
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == "1":
+                    dfs(r,c)
                     count += 1
         return count
