a265d537256603103fbb02aa61f9c49243dd7c6a
Daniel Lam <dltlam1987@gmail.com>
2025-09-16 14:44:00 -0700

    Time: 169 ms (54.5%), Space: 30.9 MB (86.66%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.py
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.py
@@ -1,10 +1,31 @@
+from collections import deque
+
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        
+        monotonic_stack = deque() # to store indices of our window
+        # maximum values at the front
+        # minimum values at the end of the deque
         result = []
-        for i in range(len(nums) - k + 1):
-            window_max = nums[i]
-            for j in range(i, i + k):
-                if nums[j] > window_max:
-                    window_max = nums[j]
-            result.append(window_max)
+
+        for r in range(len(nums)):
+            # Boundary check to remove elements that are outside of our
+            # sliding window
+            if monotonic_stack and monotonic_stack[0] < r - k + 1:
+                monotonic_stack.popleft()
+
+            # Remove elements that are smaller than current
+            # shrink our queue based on the current values that are smaller
+            while monotonic_stack and nums[monotonic_stack[-1]] <= nums[r]:
+                monotonic_stack.pop()
+
+            # Append 
+            monotonic_stack.append(r)
+
+            # Check if we're within window size and append to the result
+            if r >= k - 1:
+                result.append(nums[monotonic_stack[0]])
         return result
+
+
+
