58678cefac3791f33cb07dc5b065a3fdabf04116
Daniel Lam <dltlam1987@gmail.com>
2025-09-17 14:35:13 -0700

    Time: 68 ms (99.01%), Space: 26.2 MB (99.23%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -1,26 +1,23 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        n = len(temperatures)
-        ans = [0] * n
-        hottest = 0  # Track the hottest temperature seen so far (moving backwards)
+        # 2 pointers solution
+        # iterate backwards since we don't worry about going out of bounds
+        # jumping indexes 
+        result = [0] * len(temperatures)
+        hottest = 0
+        # hottest = temperatures[len(temperatures)- 1]
 
-        # Traverse from the last day to the first
-        for i in range(n - 1, -1, -1):
-            # If today's temp is >= anything in the future, no warmer day exists
+
+        for i in range(len(temperatures)-1, -1, -1):
             if temperatures[i] >= hottest:
                 hottest = temperatures[i]
                 continue
 
-            # Otherwise, there *is* a warmer day ahead
             j = i + 1
-            # Jump ahead until we find a warmer temperature
-            # Analogy to "Jump Game":
-            #   - In Jump Game, nums[i] tells you how far you can skip.
-            #   - Here, ans[j] tells you how many days to skip forward.
+
             while temperatures[j] <= temperatures[i]:
-                j += ans[j]  # use precomputed answers to skip ahead efficiently
+                j += result[j]
 
-            # The distance to the next warmer day is how far we jumped
-            ans[i] = j - i
+            result[i] = j - i
 
-        return ans
+        return result
