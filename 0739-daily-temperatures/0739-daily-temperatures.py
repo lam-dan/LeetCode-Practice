a6160a9696358e9040a6960bf934bb54d17ab1db
Daniel Lam <dltlam1987@gmail.com>
2025-09-17 14:41:09 -0700

    Time: 76 ms (96.76%), Space: 26.4 MB (97.47%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -4,19 +4,14 @@ class Solution:
         # iterate backwards since we don't worry about going out of bounds
         # jumping indexes 
         result = [0] * len(temperatures)
-        # hottest = 0
         hottest = temperatures[len(temperatures)- 1]
 
         for i in range(len(temperatures) - 2, -1, -1):
             if temperatures[i] >= hottest:
                 hottest = temperatures[i]
                 continue
-
             j = i + 1
-
             while temperatures[j] <= temperatures[i]:
                 j += result[j]
-
             result[i] = j - i
-
         return result
