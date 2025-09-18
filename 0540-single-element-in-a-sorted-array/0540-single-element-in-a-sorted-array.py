7a1781392aff9bcee1b39a3a188106a5dbca930e
Daniel Lam <dltlam1987@gmail.com>
2025-09-18 13:55:33 -0700

    Time: 0 ms (100%), Space: 24.7 MB (82.12%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.py
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.py
@@ -4,17 +4,21 @@ class Solution:
         right = len(nums) - 1
 
         while left < right:
+
             mid = (left + right) // 2
+
+            if nums[mid] != nums[mid - 1] and nums[mid] != nums[mid + 1]:
+                return nums[mid]
+
             if nums[mid] == nums[mid + 1]:
                 if (right - mid) % 2 == 0: # Check if you have even length
                     left = mid + 2
                 else:
                     right = mid - 1
-            elif nums[mid] == nums[mid - 1]:
-                if (right - mid) % 2 == 0:
+            else:
+                if (right - mid) % 2 == 0: # Check if you have even length
                     right = mid - 2
                 else:
                     left = mid + 1
-            else:
-                return nums[mid]
+
         return nums[left]
