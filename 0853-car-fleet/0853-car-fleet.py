b206cc1cfd4d253c30ee04ed27b7126cb3774ee7
Daniel Lam <dltlam1987@gmail.com>
2025-09-25 02:27:00 -0700

    Time: 166 ms (65%), Space: 39.2 MB (57.76%) - LeetHub

--- 0853-car-fleet/0853-car-fleet.py
+++ 0853-car-fleet/0853-car-fleet.py
@@ -5,10 +5,9 @@ class Solution:
         last_time = 0   # arrival time of the last fleet leader
 
         for pos, spd in cars:
-            t = (target - pos) / spd
-            if t > last_time:     # new fleet forms
+            time_to_target = (target - pos) / spd
+            if time_to_target > last_time:     # new fleet forms
                 fleets += 1
-                last_time = t     # update last fleet's arrival time
+                last_time = time_to_target     # update last fleet's arrival time
             # else: merges into the last fleet → do nothing
-
         return fleets
