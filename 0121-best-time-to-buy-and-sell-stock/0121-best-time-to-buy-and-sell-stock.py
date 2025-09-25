70622c322101510617a12cca11fc673eb04269c0
Daniel Lam <dltlam1987@gmail.com>
2025-09-24 22:17:19 -0700

    Time: 139 ms (15.23%), Space: 27 MB (8.16%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,17 +1,15 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
+        # Greedy Approach
         min_price = float('inf')
         max_profit = 0
-
         for i in range(len(prices)):
-            if prices[i] < min_price:
-                min_price = prices[i]
-            elif prices[i] - min_price > max_profit:
-                max_profit = prices[i] - min_price
+            max_profit = max(max_profit, prices[i] - min_price)
+            min_price = min(min_price, prices[i])
         return max_profit
 
         # Time Complexity is O(n)
         # Space Complexity is O(1) no extra space created
-        
+
 
     
