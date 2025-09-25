88ea8b2bbac60bea066b0b0bd3c309f3dec272df
Daniel Lam <dltlam1987@gmail.com>
2025-09-25 00:38:11 -0700

    Time: 76 ms (96.74%), Space: 26.4 MB (97.36%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -1,21 +1,17 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        # Monotic Decreasing Stack, as we add indices in the stack, the stack
-        # is sorted in a descending order
-        n = len(temperatures)
-        ans = [0] * n
-        stack = []  # stack stores indices of days that are still "unresolved"
-                    # i.e., we haven't yet found a warmer temperature for them
-        # Traverse from left to right
-        for i, temp in enumerate(temperatures):
-            # While the stack is not empty AND today's temperature is warmer
-            # than the temperature at the index on top of the stack...
-            while stack and temperatures[stack[-1]] < temp:
-                j = stack.pop()
-                # We found the next warmer day for day j
-                ans[j] = i - j   # distance between days
-                # Example: if temps[j] = 71 and temps[i] = 72, then ans[j] = i-j
-            # Push the current index onto the stack because we don't yet know
-            # when (or if) a warmer day will come for today.
-            stack.append(i)
-        return ans
+        # 2 pointers solution
+        # iterate backwards since we don't worry about going out of bounds
+        # jumping indexes 
+        result = [0] * len(temperatures)
+        hottest = temperatures[len(temperatures)- 1]
+
+        for i in range(len(temperatures) - 2, -1, -1):
+            if temperatures[i] >= hottest:
+                hottest = temperatures[i]
+                continue
+            j = i + 1
+            while temperatures[j] <= temperatures[i]:
+                j += result[j]
+            result[i] = j - i
+        return result
