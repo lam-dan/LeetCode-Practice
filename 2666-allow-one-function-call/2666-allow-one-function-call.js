ba971e467fd6454203eb608ff4b2be898d202df2
Daniel Lam <dltlam1987@gmail.com>
2025-10-04 23:11:27 -0700

    Time: 56 ms (5.31%), Space: 53.6 MB (56.28%) - LeetHub

---
+++ 2666-allow-one-function-call/2666-allow-one-function-call.js
@@ -0,0 +1,29 @@
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+function once(fn){
+    let hasBeenCalled = false
+    return function(...args){
+        if (hasBeenCalled) {
+            return undefined
+        } else {
+            hasBeenCalled = true;
+            return fn(...args)
+        }
+    }
+}
+
+
+
+let fn = (a,b,c) => (a + b + c)
+let onceFn = once(fn)
+onceFn(1,2,3) //6
+
+/**
+ * let fn = (a,b,c) => (a + b + c)
+ * let onceFn = once(fn)
+ *
+ * onceFn(1,2,3); // 6
+ * onceFn(2,3,6); // returns undefined without calling fn
+ */
