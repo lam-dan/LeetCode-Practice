549ca6fbd6bd4b4886a5392b788d68ed303095d5
Daniel Lam <dltlam1987@gmail.com>
2025-10-05 03:10:40 -0700

    Time: 51 ms (52.56%), Space: 54.6 MB (28.41%) - LeetHub

--- 2637-promise-time-limit/2637-promise-time-limit.js
+++ 2637-promise-time-limit/2637-promise-time-limit.js
@@ -5,19 +5,11 @@
  */
 var timeLimit = function(fn, t) {
     return async function(...args) {
-        return new Promise( async (resolve, reject) => {
-            const timeout = setTimeout(() => {
-                reject("Time Limit Exceeded")
-            }, t)
-
-            try {
-                const result = await fn(...args)
-                resolve(result)
-            } catch (err) {
-                reject(err)
-            }
-            clearTimeout(timeout)
+        const timeLimitPromise = new Promise((resolve, reject) => {
+            setTimeout(() => reject("Time Limit Exceeded"), t)
         })
+        const returnedPromise = fn(...args)
+        return Promise.race([timeLimitPromise, returnedPromise])
     }
 };
 
