756a2ad4a3788ee821afc29b6928b2a68aac2e90
Daniel Lam <dltlam1987@gmail.com>
2025-10-04 22:45:47 -0700

    Time: 52 ms (72.21%), Space: 56.7 MB (8.92%) - LeetHub

--- 2629-function-composition/2629-function-composition.js
+++ 2629-function-composition/2629-function-composition.js
@@ -4,6 +4,7 @@
  */
 var compose = function(functions) {
     return function(x) {
+        if (functions.length === 0) return x
         let accum = x
         for(let i = functions.length-1; i >= 0; i--){
             accum = functions[i](accum)
@@ -16,3 +17,7 @@ var compose = function(functions) {
  * const fn = compose([x => x + 1, x => 2 * x])
  * fn(4) // 9
  */
+
+let functions = [ x => x + 1,  x => x * x, x => x * 2]
+ const fn = compose(functions)
+ fn(4)
