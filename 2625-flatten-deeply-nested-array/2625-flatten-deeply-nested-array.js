2cd02483d9ecf855c977aa317912d00f9dec7e82
Daniel Lam <dltlam1987@gmail.com>
2025-10-06 11:48:36 -0700

    Time: 147 ms (42.69%), Space: 92.5 MB (30.54%) - LeetHub

--- 2625-flatten-deeply-nested-array/2625-flatten-deeply-nested-array.js
+++ 2625-flatten-deeply-nested-array/2625-flatten-deeply-nested-array.js
@@ -4,15 +4,18 @@
  * @return {Array}
  */
 var flat = function (arr, n) {
-    if (n === 0) return arr
-    let res = [];
-    for (const ele of arr) {
-        if (Array.isArray(ele)) {
-            res.push(...flat(ele, n-1))
-        }
-        else {
-            res.push(ele)
+    // Base Case
+    if (n === 0) {
+        return arr
+    }
+    let result = []
+    for (let i = 0; i < arr.length; i++) {
+        const obj = arr[i]
+        if (Array.isArray(obj)){
+            result.push(...flat(obj, n - 1))
+        } else {
+            result.push(arr[i])
         }
     }
-    return res
+    return result
 };
