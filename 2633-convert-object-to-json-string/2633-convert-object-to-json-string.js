c8db57794818253655ab7940448e526eaa5b3cfa
Daniel Lam <dltlam1987@gmail.com>
2025-10-08 14:52:10 -0700

    Time: 113 ms (5.75%), Space: 86.5 MB (13.79%) - LeetHub

--- 2633-convert-object-to-json-string/2633-convert-object-to-json-string.js
+++ 2633-convert-object-to-json-string/2633-convert-object-to-json-string.js
@@ -3,53 +3,24 @@
  * @return {string}
  */
 var jsonStringify = function(object) {
-    // Base Cases - Recursive Approach
-    // Objects - checked
-    // Arrays - Check
-    // Primitives - check
-    // null - check
-    // undefined - check
-    // NaN - check
-
-    // Handle Falsies
-    // Convert all the objects into Strings with double quotes
-    // "null", "undefined", "1" will all convert to null, undefined, and 1 since we 
-    // are using template literals.
-    if (object === null) return String(object)
-    if (object === undefined) return String(object)
-    if (Number.isNaN(object)) return String(object)
-
-    // Handle Primitives - Strings, Booleans, Numbers
-    if (typeof object === "boolean") {
-        return String(object)
-    } 
-    // Handle Edge case where String conversion of a string "str" => str
-    // this will break test cases where double quotes are expected of a string.
-    if (typeof object === "string") {
-        console.log("String", object)
-        // return `"${object}"`; // handles strings
-        // String("str") conversion returns just str
-        return `"${String(object)}"`
-
-    }
-    if (typeof object !== "object") { 
-        // console.log("handle primitives", object)
-        return String(object)
+    if (object === null){
+        return "null"
     }
 
-    // Handle Arrays {}, since Arrays are technically objects as well, it goes before objects
-    if (Array.isArray(object)){
-        // console.log("Crazy nested conversion", `[${obj.map(convertObjectToString).join(",")}]`)
-        return `[${object.map(jsonStringify).join(",")}]`
+    if (Array.isArray(object)) {
+        const elements = object.map((element) => jsonStringify(element))
+        return `[${elements.join(',')}]`
     }
 
-    //Handle Objects {}
-    // Recursively Handling Object Conversion to String 
-    if (typeof object === "object"){
-        const entries = Object.entries(object).map(([key, value]) => {
-            return `"${key}":${jsonStringify(value)}` // Recursion handles nested objects
-        })
-        return `{${entries.join(",")}}`
+    if (typeof object === "object") {
+        const result = Object.entries(object).map(([key, value]) =>
+            `"${key}":${jsonStringify(value)}`);
+        return `{${result.join(',')}}`;
     }
-    return false
+
+    if (typeof object === 'string') {
+        return `"${object}"`
+    }
+    return String(object)
+    
 };
