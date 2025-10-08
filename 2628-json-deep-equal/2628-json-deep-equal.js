8c6b757831162e5ef488e9b20d9b890eb836b70a
Daniel Lam <dltlam1987@gmail.com>
2025-10-08 03:51:27 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2628-json-deep-equal/2628-json-deep-equal.js
+++ 2628-json-deep-equal/2628-json-deep-equal.js
@@ -5,7 +5,7 @@
  */
 var areDeeplyEqual = function(o1, o2) {
     // Hanle Strict Equalness Early on
-  if (o1 === o2) return true;
+//   if (o1 === o2) return true;
   if (Number.isNaN(o1) && Number.isNaN(o2)) return true; // Handles cases where both are NaN
 
   // Handle null and undefined
