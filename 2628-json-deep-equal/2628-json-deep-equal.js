b58f989ddfb63da3e290343feda9303c54fd6155
Daniel Lam <dltlam1987@gmail.com>
2025-10-08 03:28:00 -0700

    Time: 69 ms (23.33%), Space: 62.7 MB (25%) - LeetHub

--- 2628-json-deep-equal/2628-json-deep-equal.js
+++ 2628-json-deep-equal/2628-json-deep-equal.js
@@ -16,9 +16,13 @@ var areDeeplyEqual = function(o1, o2) {
   if (a1 !== a2) return false;
 
   if (a1 && a2) {
-    if (o1.length !== o2.length) return false;
+    if (o1.length !== o2.length) {
+        return false;
+    }
     for (let i = 0; i < o1.length; i++) {
-      if (!areDeeplyEqual(o1[i], o2[i])) return false;
+      if (!areDeeplyEqual(o1[i], o2[i])) {
+        return false;
+      }
     }
     return true;
   }
