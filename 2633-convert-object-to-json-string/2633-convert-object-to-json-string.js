2e0a4ed4ae24a9374dac24fd029ebf8f2c2f7b84
Daniel Lam <dltlam1987@gmail.com>
2025-10-08 13:48:03 -0700

    Time: 89 ms (37.93%), Space: 79.5 MB (64.37%) - LeetHub

--- 2633-convert-object-to-json-string/2633-convert-object-to-json-string.js
+++ 2633-convert-object-to-json-string/2633-convert-object-to-json-string.js
@@ -3,33 +3,37 @@
  * @return {string}
  */
 var jsonStringify = function(object) {
-    if (object === null) return "null"
-    if (object === undefined) return "undefined"
-    if (Number.isNaN(object)) return "NaN"
-    if (typeof object === "boolean") {
-        return object
-    }
-
-    if (typeof object === "string") {
-        // console.log("String", object)
-        return `"${object}"`; // handles strings
-    }
-    if (typeof object !== "object") { //handles primitives
-        // console.log("handle primitives", object)
-        return String(object)
-    }
-    if (Array.isArray(object)){
-        // console.log("Crazy nested conversion", `[${obj.map(convertObjectToString).join(",")}]`)
-        return `[${object.map(jsonStringify).join(",")}]`
-    }
-    // Base Cases
+    // Base Cases - Recursive Approach
     // Objects - checked
-    // Arrays
+    // Arrays - Check
     // Primitives - check
     // null - check
     // undefined - check
     // NaN - check
 
+    // Handle Falsies
+    if (object === null) return "null" 
+    if (object === undefined) return "undefined"
+    if (Number.isNaN(object)) return "NaN"
+
+    // Handle Primitives - Strings, Booleans, Numbers
+    if (typeof object === "boolean") return object
+    if (typeof object === "string") {
+        // console.log("String", object)
+        return `"${object}"`; // handles strings
+    }
+    if (typeof object !== "object") { 
+        // console.log("handle primitives", object)
+        return String(object)
+    }
+
+    // Handle Arrays {}, since Arrays are technically objects as well, it goes before objects
+    if (Array.isArray(object)){
+        // console.log("Crazy nested conversion", `[${obj.map(convertObjectToString).join(",")}]`)
+        return `[${object.map(jsonStringify).join(",")}]`
+    }
+
+    //Handle Objects {}
     // Recursively Handling Object Conversion to String 
     if (typeof object === "object"){
         const entries = Object.entries(object).map(([key, value]) => {
