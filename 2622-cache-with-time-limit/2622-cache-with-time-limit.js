97148a4e118b48f40735c4b33caf024da470675a
Daniel Lam <dltlam1987@gmail.com>
2025-10-10 03:13:44 -0700

    Time: 44 ms (79.44%), Space: 54.3 MB (19.49%) - LeetHub

--- 2622-cache-with-time-limit/2622-cache-with-time-limit.js
+++ 2622-cache-with-time-limit/2622-cache-with-time-limit.js
@@ -1,17 +1,9 @@
-var TimeLimitedCache = function() {
-    this.cache = new Map()
-};
+class TimeLimitedCache {
+    constructor() {
+        this.cache = new Map();
+    }
 
-// set(key, value, duration): accepts an integer key, an integer value, and a duration in milliseconds. 
-// Once the duration has elapsed, the key should be inaccessible. The method should return true if the same un-expired key already exists and false otherwise. Both the value and duration should be overwritten if the key already exists.
-
-/** 
- * @param {number} key
- * @param {number} value
- * @param {number} duration time until expiration in ms
- * @return {boolean} if un-expired key already existed
- */
-TimeLimitedCache.prototype.set = function(key, value, duration) {
+  set(key, value, duration) {
     const valueInCache = this.cache.get(key);
     if (valueInCache) {
       clearTimeout(valueInCache.timeout);
@@ -19,31 +11,13 @@ TimeLimitedCache.prototype.set = function(key, value, duration) {
     const timeout = setTimeout(() => this.cache.delete(key), duration);
     this.cache.set(key, { value, timeout });
     return Boolean(valueInCache);
-};
+  }
 
-/** 
- * @param {number} key
- * @return {number} value associated with key
- */
-TimeLimitedCache.prototype.get = function(key) {
-    if (this.cache.has(key)){
-        return this.cache.get(key).value
-    } else {
-        return -1
-    }
-};
+  get(key) {
+    return this.cache.has(key) ? this.cache.get(key).value : -1;
+  }
 
-/** 
- * @return {number} count of non-expired keys
- */
-TimeLimitedCache.prototype.count = function() {
-    return this.cache.size
-    
+  count() {
+    return this.cache.size;
+  }
 };
-
-/**
- * const timeLimitedCache = new TimeLimitedCache()
- * timeLimitedCache.set(1, 42, 1000); // false
- * timeLimitedCache.get(1) // 42
- * timeLimitedCache.count() // 1
- */
