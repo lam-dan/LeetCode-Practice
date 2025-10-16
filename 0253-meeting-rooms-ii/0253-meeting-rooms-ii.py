5a3933c3e368b77b4ed854cab34c3e8c0e257e34
Daniel Lam <dltlam1987@gmail.com>
2025-10-16 04:07:35 -0700

    Time: 6 ms (38.61%), Space: 19.9 MB (51.14%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
@@ -2,26 +2,16 @@ import heapq
 
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
-        # iterate through values and compare current start time vs previous end time
-        if not intervals:
-            return 0
-        # sort the intervals by start time
-        intervals.sort(key = lambda a: a[0]) # Sort by start time
+        intervals.sort(key = lambda a: a[0])
         heap = []
 
-        for start, end in intervals:
-            # Compare the current node in the intervals to what was previously pushed into our heap
-            if heap and start >= heap[0][0]: # Curr start time compared against Prev end time
-                heapq.heappop(heap) # pop the value since we need to free up the room
-            # Push into our heap the end times only
-            heapq.heappush(heap, (end, start)) # 1) First heap push will be the first intervals
+        for i in range(len(intervals)):
+            start = intervals[i][0]
+            end = intervals[i][1]
+            if heap and start >= heap[0][0]:
+                heapq.heappop(heap)
+            heapq.heappush(heap, (end, start))
         return len(heap)
-            
-        
-
-        
-
-
 
 
 
