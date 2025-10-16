ea3371cc3b2ad0031ec6e19a474e2d8a53137b8e
Daniel Lam <dltlam1987@gmail.com>
2025-10-16 02:30:59 -0700

    Time: 3545 ms (68.06%), Space: 17.8 MB (65.18%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -2,35 +2,33 @@ class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         rows = len(board)
         cols = len(board[0])
-        path = set()
-
-        def dfs(i, j, k):
-            if k == len(word):
-                return True
+        # Backtracking algorithm
+        def dfs(r, c, i):
             # Base Case
+            if len(word) == i:
+                return True
+            # Falsy Check - Out of bounds, or visited
             if (
-                i < 0 or 
-                j < 0 or
-                i >= rows or
-                j >= cols or # Boundaries of matrix
-                board[i][j] != word[k] or  # Not the same word
-                (i, j) in path # not in path
+                r < 0 or r >= rows or c < 0 or c >= cols or
+                board[r][c] == 0 or board[r][c] != word[i]
             ):
                 return False
-            path.add((i,j))
-            #Check 4 directions, always increment k to update word[k]
+            # Mark as visited
+            board[r][c] = 0
+            
             res = (
-                dfs(i + 1, j, k + 1) or
-                dfs(i, j + 1, k + 1) or
-                dfs(i - 1, j, k + 1) or
-                dfs(i, j - 1, k + 1) 
+                dfs(r + 1, c, i + 1) or
+                dfs(r - 1, c, i + 1) or
+                dfs(r, c + 1, i + 1) or
+                dfs(r, c - 1, i + 1)
             )
-            #Backtrack
-            path.remove((i,j))
+            # Back track if after searching in remaing directions and the next character
+            # of the word doesn't exist
+            board[r][c] = word[i]
             return res
 
-        for i in range(rows):
-            for j in range(cols):
-                if dfs(i, j, 0): # k=0 to track index of char in word
+        for r in range(rows):
+            for c in range(cols):
+                if dfs(r,c,0):
                     return True
         return False
