629a7d06cc401a021a738a6fa62f100ec4931cfb
Daniel Lam <dltlam1987@gmail.com>
2025-10-16 04:11:06 -0700

    Time: 4 ms (54.83%), Space: 20 MB (51.14%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
@@ -1,10 +1,8 @@
 import heapq
-
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
-        intervals.sort(key = lambda a: a[0])
+        intervals.sort(key = lambda x: x[0])
         heap = []
-
         for i in range(len(intervals)):
             start = intervals[i][0]
             end = intervals[i][1]
@@ -13,5 +11,4 @@ class Solution:
             heapq.heappush(heap, (end, start))
         return len(heap)
 
-
-
+        
