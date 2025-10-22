d3009d7989cbee60952f34e6c8bfa23498b3278e
Daniel Lam <dltlam1987@gmail.com>
2025-10-22 05:12:39 -0700

    Time: 35 ms (56.2%), Space: 19.2 MB (24.77%) - LeetHub

--- 0846-hand-of-straights/0846-hand-of-straights.py
+++ 0846-hand-of-straights/0846-hand-of-straights.py
@@ -14,3 +14,7 @@ class Solution:
                     if counter[j] < 0:
                         return False
         return True
+
+        # Time Complexity is O(n*logn + n * k) n is the number of numbers in the hand, and k is the groupSize
+        # of each hand since we have a nested for loop.
+        # Space Complexity is O(n) since we create hash map to count the frequency
