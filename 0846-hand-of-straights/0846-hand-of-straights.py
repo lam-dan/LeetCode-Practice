a76da4361ad30d044679f90416a4119ed4f76c83
Daniel Lam <dltlam1987@gmail.com>
2025-10-22 05:03:23 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0846-hand-of-straights/0846-hand-of-straights.py
+++ 0846-hand-of-straights/0846-hand-of-straights.py
@@ -1,39 +1,14 @@
 class Solution:
     def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
-        n = len(hand)
-
-        if n % groupSize != 0:
+        N = len(nums)
+        if N % size:
             return False
-
-        counter = Counter(hand)
-        sorted_counter_list = sorted(counter) # Converts freq map into a list sorted by key
-        # print(sorted(cnt, key=lambda x: cnt[x])) - if you want to sorted it by value
-
-        for i in range(len(sorted_counter_list)): # Sort unique values from counter
-            num = sorted_counter_list[i]
-
-            if num not in counter:            # already consumed by earlier steps
-                continue
-
-            count = counter[num]
-            
-            
-            for k in range(num, num + groupSize): # Example: Loop 1 -> 1 + 3 = 4, so loop 1,2,3
-                if k not in counter:
-                    return False
-
-                if counter[k] < count:
-                    return False
-
-                counter[k] -= count
-
-                if counter[k] == 0:
-                    del counter[k]
+        freq = Counter(nums)
+        nums.sort()
+        for num in nums:
+            if freq[num]:
+                for cur in range(num, num + size):
+                    freq[cur] -= 1
+                    if freq[cur] < 0:
+                        return False
         return True
-                
-
-                
-
-
-
-
